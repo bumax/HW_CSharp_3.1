891a2b33bf4c3f04179f5244af9f7318fee4976e
user <user@WIN-AKH758KNU81>
2023-11-19 18:14:37 +1000

    Добавьте файлы проекта.

---
+++ FamilyMember.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_CSharp_3._1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class FamilyMember
+    {
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+
+        void Info(int indent = 0)
+        {
+            Console.WriteLine($"{new String('-', indent)}Имя {this.Name}");
+        }
+        public virtual void Print(int indent = 0)
+        {
+            Info(indent);
+        }
+        public void PrintTree()
+        {
+            PrintTreeRecurcive(this, Console.WindowWidth/2);
+        }
+        private void PrintTreeRecurcive(FamilyMember memeber, int x, int y = 0)
+        {
+            int n = Convert.ToInt32(Console.WindowWidth / (Math.Pow(2.0, Convert.ToDouble(y+1)) + 1 ) / 2);
+            Console.SetCursorPosition(x, y);
+            Console.Write(memeber.Name);
+            if (memeber.Father.Name != null)
+            {
+                Console.SetCursorPosition(x - 1, y);
+                Console.Write('┌');
+                PrintTreeRecurcive(memeber.Father, x - n, y + 1);
+            }
+            if (memeber.Mother.Name != null)
+            {
+                Console.SetCursorPosition(x + memeber.Name.Length, y);
+                Console.Write('┐');
+                PrintTreeRecurcive(memeber.Mother, x + n, y + 1);
+            }
+        }
+
+
+    }
+    public class AdultFamilyMember : FamilyMember
+    {
+        public FamilyMember[] Children { get; set; }
+        public override void Print(int indent = 0)
+        {
+            base.Print(indent);
+
+            foreach (var child in Children)
+                child.Print(indent * 2);
+        }
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using HW_CSharp_3._1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FamilyMember tree = new FamilyMember();
+        GenerateRandomTree(3, tree);
+        tree.PrintTree();
+    }
+    
+    private static void GenerateRandomTree(int depth, FamilyMember member) { 
+        if (depth > 0)
+        {
+            Random rnd = new Random();
+            depth--;
+            member.Father = new FamilyMember();
+            member.Mother = new FamilyMember();
+            FamilyMember father = member.Father;
+            FamilyMember mother = member.Mother;
+            member.Name = "Name" + rnd.Next().ToString();
+            father.Sex = Gender.Male;
+            mother.Sex = Gender.Female;
+            GenerateRandomTree(depth, father);
+            GenerateRandomTree(depth, mother);
+        }
+    
+    }
+}
